0ce58221971b19469db57867b9943c5361e26198
Will Pracht <willpracht@gmail.com>
2012-06-02 17:26:32 -0500

    Basics
    Initial styles, layout for conversation view, etc.

---
+++ footer.php
@@ -0,0 +1,7 @@
+
+	<script type="text/javascript" src="#"></script>
+	<script type="text/javascript" src="#"></script>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+
+<head>
+
+	<title>Cyrano</title>
+
+	<link type="text/css" rel="stylesheet" href="style.css" />
+
+</head>
+
+<body>
