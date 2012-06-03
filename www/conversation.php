e78583fa58ec543dc9711e47b5b82d5a4d3311fb
Will Pracht <willpracht@gmail.com>
2012-06-02 20:10:59 -0500

    Uh…
    Same shit I just said, asshole.

--- www/conversation.php
+++ www/conversation.php
@@ -4,7 +4,42 @@
 
 		<div class="logo"></div>
 
-		<textarea></textarea>
+		<h2>she cried when i left for the hackathon</h2>
+		<h3>Submitted June 2, 2012 @ 6:47PM CDT</h3>
+
+		<div class="personA person">
+			
+			Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+
+			<div class="datetime">June 1, 2012 @ 10:00PM CDT</div>
+
+		</div>
+
+		<div class="personB person">
+			
+			Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
+
+			<div class="datetime">June 1, 2012 @ 10:00PM CDT</div>
+
+		</div>
+
+		<div class="personA person">
+			
+			Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+
+			<div class="datetime">June 1, 2012 @ 10:00PM CDT</div>
+
+		</div>
+
+		<div class="personB person">
+			
+			Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. 
+
+			<div class="datetime">June 1, 2012 @ 10:00PM CDT</div>
+
+		</div>
+
+		<textarea>Tell that bitch to be cool. Be cool, bitch!</textarea>
 		<button>Comment</button>
 
 	</div>
