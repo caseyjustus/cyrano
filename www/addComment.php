acca7ae4d8f1f29119675c81152b7dda281c3abd
Caleb Kniffen <c_kniffen@kniffenwebdesign.com>
2012-06-03 09:35:08 -0500

    Added add comment call.

--- www/addComment.php
+++ www/addComment.php
@@ -1,2 +1,10 @@
 <?php
+	require_once('master.php');
+	
+	$sql = "INSERT INTO comments (texts_id, text, vote) VALUES (:texts_id, :text, 0)";
+	$q = $dbh->prepare($sql);
+	$q->execute(array(
+		':texts_id' => $_GET['texts_id'],
+        ':text' =>  $_GET['body']
+	));
 ?>
--- www/conversation.php
+++ www/conversation.php
@@ -1,52 +1,42 @@
 <?php
-	$conversation = 
+	$stmt = $dbh->prepare('SELECT * FROM texts WHERE id = :id');
+	$stmt->execute(array(
+		':id' => $_GET['id']
+	));
+	
+	$text = $stmt->fetch(PDO::FETCH_OBJ);
 ?>
 
-<div class="conversation panel">
+<div class="conversation panel" data-text-id="<?php print $text->id; ?>">
 
 	<div class="logo"></div>
 
-	<h2>she cried when i left for the hackathon</h2>
+	<h2><?php $text->name; ?></h2>
 	<h3>Submitted June 2, 2012 @ 6:47PM CDT</h3>
+	
+	<?php
+		$messages = json_decode($text->messages);
+		if($messages){
+			foreach($messages as $message){
+	?>
+		<div class="<?php print ($message->is_me) ? 'personA' : 'personB' ?> person">
+			
+			<?php print $message->body; ?>
 
-	<div class="personA person">
-		
-		Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.
+			<div class="datetime">June 1, 2012 @ 10:00PM CDT</div>
 
-		<div class="datetime">June 1, 2012 @ 10:00PM CDT</div>
-
-	</div>
-
-	<div class="personB person">
-		
-		Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
-
-		<div class="datetime">June 1, 2012 @ 10:00PM CDT</div>
-
-	</div>
-
-	<div class="personA person">
-		
-		Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-
-		<div class="datetime">June 1, 2012 @ 10:00PM CDT</div>
-
-	</div>
-
-	<div class="personB person">
-		
-		Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. 
-
-		<div class="datetime">June 1, 2012 @ 10:00PM CDT</div>
-
-	</div>
+		</div>
+	<?php
+			}
+		}
+	?>
 
 	<textarea>Tell that bitch to be cool. Be cool, bitch!</textarea>
 	<button>Comment</button>
 
 </div>
 
-<div class="comments panel">
+<div class="comments panel" data-text-id="<?php print $text->id; ?>">
 
 	<div class="hot group">
 		<h4>The Hotness</h4>
