6faf592608a3a4992a215d190f9eb3f4dfd42ca0
Will Pracht <willpracht@gmail.com>
2012-06-03 08:27:49 -0500

    global.js
    Removed global.js, added link to it.

--- www/footer.php
+++ www/footer.php
@@ -3,18 +3,8 @@
 </div>
 
 	<script type="text/javascript" src="scripts/jquery-1.7.2.min.js"></script>
-	<script type="text/javascript" src="scripts/jquery.isotope.min.js"></script>
-	<script type="text/javascript">
-
-		$(function(){
-
-			$('ul.main').isotope({
-  				layoutMode : 'masonry'
-			});
-
-		});
-
-	</script>
+	
+	<script type="text/javascript" src="scripts/global.js"></script>
 
 </body>
 
