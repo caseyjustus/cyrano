5ab853ffc66d963f93de2ccff1111fe8831d5dcc
Will Pracht <willpracht@gmail.com>
2012-06-03 09:06:04 -0500

    FIXES
    Links colors, isotope call again for layout.

--- www/footer.php
+++ www/footer.php
@@ -3,8 +3,17 @@
 </div>
 
 	<script type="text/javascript" src="scripts/jquery-1.7.2.min.js"></script>
-	
+	<script type="text/javascript" src="scripts/jquery.isotope.min.js"></script>
 	<script type="text/javascript" src="scripts/global.js"></script>
+
+	<script type="text/javascript">
+
+		$(function(){
+			$('ul.main').isotope();
+
+		});
+
+	</script>
 
 </body>
 
