6304f1b2b891dd4585048e2ebf1fdcaa16dad636
Will Pracht <willpracht@gmail.com>
2012-06-03 04:12:43 -0500

    Conversation Views
    Some work on conversation views.

---
+++ www/conversation-mobile.php
@@ -0,0 +1,32 @@
+<div class="mobile">
+
+	<div class="logoMobile"></div>
+
+	<h2>she cried when i left for the hackathon</h2>
+	<h3>Submitted June 2, 2012 @ 6:47PM CDT</h3>
+
+	<ul class="conversationMobile">
+
+		<li class="person personA">
+			<span>Lorem ipsum dolor sit...</span>
+			<div class="datetime">June 1, 2012 @ 10:00PM CDT</div>
+		</li>
+		<li class="person personB">
+			<span>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.</span>
+			<div class="datetime">June 1, 2012 @ 10:00PM CDT</div>
+		</li>
+		<li class="person personA">
+			<span>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</span>
+			<div class="datetime">June 1, 2012 @ 10:00PM CDT</div>
+		</li>
+
+	</ul>
+
+	<ul class="commentsMobile">
+		<li>
+			<span>Lorem ipsum dolor sit...</span>
+			<div class="datetime">June 1, 2012 @ 10:00PM CDT</div>
+		</li>
+	</ul>
+
+</div>
--- www/index.php
+++ www/index.php
@@ -2,7 +2,7 @@
 
 require_once('header.php');
 
-require_once('home.php');
+require_once('conversation-mobile.php');
 
 require_once('footer.php');
 
